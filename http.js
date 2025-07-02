9aa41baa8f086a713d28c0ad87a06563d4e2c1dc
Sahil <sk4386840@gmail.com>
2025-07-02 10:37:07 +0530

    http module

--- http.js
+++ http.js
@@ -1,3 +1,12 @@
 const http = require('http');
 
-console.log(http.STATUS_CODES);
+// console.log(http.STATUS_CODES);
+const server = http.createServer((req, res) => {
+    const {url} = req;
+    if(url === '/' && method === 'GET'){
+        res.writeHead(200, {'Content-Type': "text/plain"});
+        res.end("Hello world!");
+        console.log(req);
+    }
+})
+server.listen(5000);
